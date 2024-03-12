1cd273206fa1471495583bbf4f1958bc0ece45c9
yeongdae <oooo_0@naver.com>
2024-03-12 17:57:24 +0900

    Feat: WebConfig, WebSecurityConfig 기초 설정
    Change-Id: I2144cb01f8ba47b98d1a056e8359527ebfdbff31

---
+++ backend/src/main/java/com/ssafy/eureka/config/WebConfig.java
@@ -0,0 +1,13 @@
+package com.ssafy.eureka.config;
+
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(final CorsRegistry registry) {
+
+    }
+
+}
