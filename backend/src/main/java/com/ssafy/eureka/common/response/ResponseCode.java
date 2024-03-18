36a00f444d2c35cd2cd5e52ed92bea09b2586ae2
yeongdae <oooo_0@naver.com>
2024-03-18 17:01:11 +0900

    Feat: Util 및 공통 사항
    - ResponseCode 추가
    - 생년월일 변경 및 전화번호 암호화 Method 추가
    
    Change-Id: I4f96b8c03d9b3bac506092cbce98d6ed6eba73af

--- backend/src/main/java/com/ssafy/eureka/common/response/ResponseCode.java
+++ backend/src/main/java/com/ssafy/eureka/common/response/ResponseCode.java
@@ -13,8 +13,17 @@ public enum ResponseCode {
 
     // 유저
     USER_NOT_FOUND(HttpServletResponse.SC_NOT_FOUND, "유저가 존재하지 않습니다"),
+    USER_PASSWORD_ERROR(HttpServletResponse.SC_NOT_FOUND, "비밀번호 오류"),
+    USER_BIRTH_ERROR(HttpServletResponse.SC_NOT_FOUND, "주민등록번호 오류"),
 
+    // 인증 문자 에러
+    PASSWORD_ERROR(HttpServletResponse.SC_NOT_FOUND, "비밀번호 오류"),
 
+    //
+    AES_ERROR(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "비밀번호 암호화 오류"),
+
+    REFRESHTOKEN_ERROR(HttpServletResponse.SC_NOT_FOUND, "리프레시 토큰 오류"),
+    USER_ALREADY_EXSIST(HttpServletResponse.SC_NOT_FOUND, "이미 가입된 회원입니다.")
     ;
 
     private final int code;
