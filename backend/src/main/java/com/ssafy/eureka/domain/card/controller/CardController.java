d94b75d3567dee7a095e6d0e0cced54f4ea98d2c
yeongdae <oooo_0@naver.com>
2024-03-20 10:28:24 +0900

    Feat: 카드 상품 등록 시의 Card API
    Change-Id: Ibc32c68985e0785db8df6855089745e560513f73

--- backend/src/main/java/com/ssafy/eureka/domain/card/controller/CardController.java
+++ backend/src/main/java/com/ssafy/eureka/domain/card/controller/CardController.java
@@ -1,8 +1,11 @@
 package com.ssafy.eureka.domain.card.controller;
 
+import com.ssafy.eureka.common.response.ApiResponse;
+import com.ssafy.eureka.domain.card.service.CardService;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,5 +15,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @RequestMapping("/api/card")
 public class CardController {
+
+    private final CardService cardService;
+    @GetMapping("/admin/regist")
+    public ApiResponse registAllCardProduct (){
+        cardService.registAllCardProduct();
+        return ApiResponse.ok("등록 성공");
+    }
 
 }
--- backend/src/main/java/com/ssafy/eureka/domain/card/service/CardServiceImpl.java
+++ backend/src/main/java/com/ssafy/eureka/domain/card/service/CardServiceImpl.java
@@ -1,8 +1,17 @@
 package com.ssafy.eureka.domain.card.service;
 
+import com.ssafy.eureka.domain.card.repository.CardRepository;
+import com.ssafy.eureka.domain.card.util.CardDataUtil;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CardServiceImpl implements CardService{
 
+    private CardRepository cardRepository;
+    private CardDataUtil cardDatautil;
+
+    @Override
+    public void registAllCardProduct() {
+        cardDatautil.cardProductFileLoad();
+    }
 }
