b6e5c664bf6f70fa5e7e1fd09def7bb6a85eb13e
김해인 <khi762@naver.com>
2024-03-21 17:05:33 +0900

    Feat: 메인 페이지 추가
    라우터 설정
    
    Change-Id: Ifd8a5ad08813bd1895423cb5952096af73737e47

---
+++ frontend-web/src/pages/MainPage.jsx
@@ -0,0 +1,10 @@
+// import apk from "../../../frontend/android/app/build/outputs/apk/release/app-release.apk"
+
+export default function MainPage() {
+  return (
+    <div>
+      <h1>Eureka</h1>
+      {/* <a href={apk}>설치하기</a> */}
+    </div>
+  )
+}
