88eae62f017f112fbc0669739d8c13e56aaa72e2
yeongdae <oooo_0@naver.com>
2024-03-22 10:59:04 +0900

    Fix: feign localhost -> server url 수정
    Change-Id: I33a7404e827d35a091ee292b8bbb9e9ab46cf8d2

--- backend/src/main/java/com/ssafy/eureka/domain/auth/jwt/CustomAuthenticationEntryPoint.java
+++ backend/src/main/java/com/ssafy/eureka/domain/auth/jwt/CustomAuthenticationEntryPoint.java
@@ -1,5 +1,6 @@
 package com.ssafy.eureka.domain.auth.jwt;
 
+import com.ssafy.eureka.common.exception.CustomException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
@@ -12,6 +13,6 @@ public class CustomAuthenticationEntryPoint implements
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response,
         AuthenticationException authException) throws IOException {
-        response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Auth Error");
+        response.sendError(HttpServletResponse.SC_UNAUTHORIZED, authException.getMessage());
     }
 }
