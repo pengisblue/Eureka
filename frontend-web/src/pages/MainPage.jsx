770c63ff2bd70ec177165847642b96566a54f1b0
yeongdae <oooo_0@naver.com>
2024-03-24 16:36:42 +0900

    Feat: QR코드 추가 ( 간단한 결제 정보 설정해놨음 )
    Change-Id: Ifd7afe37147cb401ca8a3d6a5b339b575588614b

--- frontend-web/src/pages/MainPage.jsx
+++ frontend-web/src/pages/MainPage.jsx
@@ -1,10 +1,12 @@
 // import apk from "../../../frontend/android/app/build/outputs/apk/release/app-release.apk"
+import PayQRCodeComponent from "./PayQRCodeComponent"
 
 export default function MainPage() {
   return (
     <div>
       <h1>Eureka</h1>
       {/* <a href={apk}>설치하기</a> */}
+      <PayQRCodeComponent />
     </div>
   )
 }
