02594732942665586d361fa0eae60ae2c0a4d166
oksehun <sehun9803@naver.com>
2024-03-26 11:58:02 +0900

    Feat: create fityourconsumption
    Change-Id: I523a89cad143a7cc12fc5d1e793d6430108962fd

--- frontend/src/pages/ProductPage/components/FitYourConsumption.jsx
+++ frontend/src/pages/ProductPage/components/FitYourConsumption.jsx
@@ -11,6 +11,81 @@ import {
 } from "react-native";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
 import { useNavigation } from "@react-navigation/native";
+// 데이터 더미
+const categoriesData = [
+  {
+    categoryName: "대중교통",
+    totalAmount: 100000,
+    cards: [
+      {
+        cardName: "카드X",
+        discountContent: "대중교통 10% 할인",
+        discountAmount: 10000,
+        cardImage: "../../../../assets/card2.png",
+      },
+      {
+        cardName: "카드Y",
+        discountContent: "첫 결제 시 20% 할인",
+        discountAmount: 20000,
+        cardImage: "../../../../assets/card2.png",
+      },
+      {
+        cardName: "카드Z",
+        discountContent: "버스 전용 5% 할인",
+        discountAmount: 5000,
+        cardImage: "../../../../assets/card2.png",
+      },
+    ],
+  },
+  {
+    categoryName: "쇼핑",
+    totalAmount: 200000,
+    cards: [
+      {
+        cardName: "카드A",
+        discountContent: "온라인 쇼핑몰 5% 할인",
+        discountAmount: 10000,
+        cardImage: "../../../../assets/card2.png",
+      },
+      {
+        cardName: "카드B",
+        discountContent: "특정 브랜드 10% 할인",
+        discountAmount: 20000,
+        cardImage: "../../../../assets/card2.png",
+      },
+      {
+        cardName: "카드C",
+        discountContent: "첫 구매 15% 할인",
+        discountAmount: 30000,
+        cardImage: "../../../../assets/card2.png",
+      },
+    ],
+  },
+  {
+    categoryName: "음식점",
+    totalAmount: 150000,
+    cards: [
+      {
+        cardName: "카드1",
+        discountContent: "레스토랑 10% 할인",
+        discountAmount: 15000,
+        cardImage: "../../../../assets/card2.png",
+      },
+      {
+        cardName: "카드2",
+        discountContent: "패스트푸드 5% 캐시백",
+        discountAmount: 7500,
+        cardImage: "../../../../assets/card2.png",
+      },
+      {
+        cardName: "카드3",
+        discountContent: "카페 음료 20% 할인",
+        discountAmount: 30000,
+        cardImage: "../../../../assets/card2.png",
+      },
+    ],
+  },
+];
 
 function FitYourConsumption() {
   const navigation = useNavigation();
@@ -41,12 +116,54 @@ function FitYourConsumption() {
         />
       </View>
 
-      <View style={styles.mainContent}></View>
+      {/* 나중에 분리예정 */}
+      {categoriesData.map((category, index) => (
+        <View key={index} style={styles.mainContent}>
+          <View style={styles.titleConatiner}>
+            <View style={styles.titleTextContainer}>
+              <Text style={styles.titleBest}>
+                {category.categoryName}할인 BEST
+              </Text>
+              <Text style={styles.titleSub}>
+                총 {category.totalAmount} 썼어요
+              </Text>
+            </View>
+            <Image
+              source={require("../../../../assets/favicon.png")}
+              style={styles.categoriesImage}
+            ></Image>
+          </View>
+
+          {category.cards.map((card, cardIdx) => (
+            <View key={cardIdx} style={styles.cardContainer}>
+              <Image
+                source={require("../../../../assets/card2.png")}
+                style={styles.image2}
+              />
+
+              <View style={styles.cardInfo}>
+                <Text>{card.cardName}</Text>
+                <Text>{card.discountContent}</Text>
+                <Text>{card.discountAmount} 더 할인받아요!</Text>
+              </View>
+            </View>
+          ))}
+          <View style={styles.separator}></View>
+        </View>
+      ))}
     </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
+  separator: {
+    height: 0.7,
+    width: "95%",
+    backgroundColor: "grey",
+    marginLeft: 10,
+    marginTop: 25,
+    marginBottom: 35,
+  },
   container: {
     paddingTop: StatusBar.currentHeight + 50,
   },
@@ -81,8 +198,26 @@ const styles = StyleSheet.create({
     marginLeft: 80,
     marginTop: 20,
   },
-  mainContent: {},
-  titleConatiner: {},
+  image2: {
+    height: 70,
+    width: 40,
+  },
+  mainContent: {
+    backgroundColor: "#85cf54",
+  },
+  titleConatiner: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginTop: 10,
+    marginBottom: 20,
+  },
+  titleTextContainer: {},
+  categoriesImage: {},
+  cardContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  cardInfo: {},
 });
 
 export default FitYourConsumption;
