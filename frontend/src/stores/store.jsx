b991e4449cbfa278723bc52f1b348d4cbdab6923
oksehun <sehun9803@naver.com>
2024-03-27 17:55:43 +0900

    Feat: selectCardInfo of  product-page and CardList, ChooseONe
    Change-Id: I877871c9e5ac72a93a22010ffa4a241933c71f5d

--- frontend/src/stores/store.jsx
+++ frontend/src/stores/store.jsx
@@ -1,8 +1,10 @@
-import { configureStore } from '@reduxjs/toolkit'
-import cardSlice from '../slices/cardSlice'
+import { configureStore } from "@reduxjs/toolkit";
+import cardSlice from "../slices/cardSlice";
+import productSlice from "../slices/productSlice";
 
 export const store = configureStore({
   reducer: {
     cardList: cardSlice,
+    productList: productSlice,
   },
-})
+});
