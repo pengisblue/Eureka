a0d5b5bda37259590970765b72383c206dfa15b7
kdw <hide4321@naver.com>
2024-03-28 02:31:58 +0900

    Fix: discountType 타입 변경
    Change-Id: I698f0148fc8de162f06252c881e0e595408aeeac

--- backend/src/main/java/com/ssafy/eureka/domain/card/dto/response/CardDetailBenefitList.java
+++ backend/src/main/java/com/ssafy/eureka/domain/card/dto/response/CardDetailBenefitList.java
@@ -10,7 +10,7 @@ import lombok.NoArgsConstructor;
 public class CardDetailBenefitList {
 
     String discountCostType;
-    float discountCost;
+    double discountCost;
     String largeCategoryName;
 
 }
