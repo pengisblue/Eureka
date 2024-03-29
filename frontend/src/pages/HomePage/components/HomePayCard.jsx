c22255c83b468041f1c1f9c2476e25566b317c29
이승집 <edward626kr@gmail.com>
2024-03-29 16:34:25 +0900

    Feat: 카드 디테일 결제내역 연결
    Change-Id: I40a33a83b53660be1895a330286b60ba0045c9a2

--- frontend/src/pages/HomePage/components/HomePayCard.jsx
+++ frontend/src/pages/HomePage/components/HomePayCard.jsx
@@ -1,6 +1,5 @@
-import { StyleSheet, View, Text, Pressable } from "react-native"
+import { StyleSheet, View, Text, Pressable, Image } from "react-native"
 import { MaterialCommunityIcons } from '@expo/vector-icons';
-import MyCarousel from "./MyCarousel";
 import { useNavigation } from '@react-navigation/native';
 
 function HomePayCard () {
@@ -8,16 +7,17 @@ function HomePayCard () {
   return (
     <View>
       <View style={styles.header}>
-        <Text style={styles.easyPay}>간편 결제</Text>
+        <Image source={require('../../../../assets/main.png')} style={{width: 100, height: 61, backgroundColor: '#00BFFF', borderRadius: 10, marginStart: 40}}/>
+        <View style={{marginLeft: -40}}>
+          <Text style={{fontSize: 16, fontWeight: 'bold', color: '#00BFFF'}}>당신을 위한</Text>
+          <Text style={{fontSize: 16, fontWeight: 'bold', color: '#00BFFF'}}>카드 추천 서비스</Text>
+        </View>
         <Pressable onPress={() => navigation.navigate('SettingRouters')}>
           <View style={styles.settingIcon}>
             <MaterialCommunityIcons 
               name="cog-outline" size={36} color={'#9E9797'}/>
           </View>
         </Pressable>
-      </View>
-      <View style={styles.carousel}>
-        <MyCarousel />
       </View>
     </View>
   )
@@ -30,15 +30,17 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     justifyContent: 'space-between',
     marginBottom: 12,
+    alignItems:'center'
   },
   easyPay: {
-    fontSize: 24,
+    fontSize: 30,
     fontWeight: 'bold',
-    marginLeft: 12,
+    marginLeft: 50,
+    color: '#6797ff'
   },
   settingIcon: {
     alignSelf: 'center',
-    marginRight: 12,
+    marginRight: 20,
   },
   carousel: {
     marginBottom: 12,
