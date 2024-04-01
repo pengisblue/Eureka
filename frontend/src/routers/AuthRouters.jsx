456f88252a00b4cf60ceb38d75c9b81e27b8f372
최용훈 <cyh2607@gmail.com>
2024-04-02 01:24:34 +0900

    Feat:비밀번호 저장 및 지문인식상태관리
    Change-Id: Ib254b8cb151044f11d2e5d7890cd5d265ecf3aa2

--- frontend/src/routers/AuthRouters.jsx
+++ frontend/src/routers/AuthRouters.jsx
@@ -5,7 +5,12 @@ import PasswordConfirmPage from '../pages/SignUpPage/PasswordConfirmPage'
 import Routers from "./Routers";
 import LoadingPage from "../pages/LoadingPage/LoadingPage"
 import SplashPage from "../pages/LoadingPage/SplashPage";
-
+import ConfirmLoading from "../pages/SettingPage/components/ConfirmLoading";
+import PasswordChange from "../pages/SettingPage/components/PasswordChange";
+import PasswordChangeConfirm from "../pages/SettingPage/components/PasswordChangeConfirm";
+import VerifyPasswordChange from "../pages/SettingPage/components/VerifyPasswordChange";
+import SignupPasswordChange from "../pages/SignUpPage/components/SignupPasswordChange";
+import SignupPasswordChangeConfirm from "../pages/SignUpPage/components/SignupPasswordChangeConfirm";
 
 
 function AuthenticationRouter () {
@@ -18,6 +23,12 @@ function AuthenticationRouter () {
       <AuthStack.Screen name="PasswordPage" component={PasswordPage}/>
       <AuthStack.Screen name="PasswordConfirmPage" component={PasswordConfirmPage}/>
       <AuthStack.Screen name='SplashPage' component={SplashPage}/>
+      <AuthStack.Screen name='CheckModal' component={ConfirmLoading}/>
+      <AuthStack.Screen name="VerifyPasswordChange" component={VerifyPasswordChange}/>
+      <AuthStack.Screen name="PasswordChange" component={PasswordChange}/>
+      <AuthStack.Screen name="PasswordChangeConfirm" component={PasswordChangeConfirm}/>
+      <AuthStack.Screen name='SignupPasswordChange' component={SignupPasswordChange}/>
+      <AuthStack.Screen name='SignupPasswordChangeConfirm' component={SignupPasswordChangeConfirm}/>
       <AuthStack.Screen name='Routers' component={Routers}/>
     </AuthStack.Navigator>
   )
