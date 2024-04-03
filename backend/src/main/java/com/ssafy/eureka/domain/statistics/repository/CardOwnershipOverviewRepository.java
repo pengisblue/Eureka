1e12a8a1482f4c0941b150a008e99e624b2e0e32
kdw <hide4321@naver.com>
2024-04-03 16:17:16 +0900

    Fix: 많이 보유한 카드 10개에 Info 추가
    Change-Id: Ia843ba833d6e025a022aad7ffca4d7f45b75318e

--- backend/src/main/java/com/ssafy/eureka/domain/statistics/repository/CardOwnershipOverviewRepository.java
+++ backend/src/main/java/com/ssafy/eureka/domain/statistics/repository/CardOwnershipOverviewRepository.java
@@ -13,8 +13,16 @@ import java.util.List;
 @Repository
 public interface CardOwnershipOverviewRepository extends JpaRepository<CardOwnershipOverviewEntity, Integer> {
 
-    @Query("SELECT new com.ssafy.eureka.domain.statistics.dto.CardOwnershipDto(co.cardId, c.cardName, c.imagePath, c.imgAttr, co.ownershipCount) " +
-            "FROM CardOwnershipOverviewEntity co JOIN CardEntity c ON co.cardId = c.cardId " +
+//    @Query("SELECT new com.ssafy.eureka.domain.statistics.dto.CardOwnershipDto(co.cardId, c.cardName, c.imagePath, c.imgAttr, co.ownershipCount) " +
+//            "FROM CardOwnershipOverviewEntity co JOIN CardEntity c ON co.cardId = c.cardId " +
+//            "WHERE co.createdDate = :date " +
+//            "ORDER BY co.ownershipCount DESC LIMIT 10")
+//    List<CardOwnershipDto> findCardOwnershipOverviews(@Param("date") LocalDate date);
+
+    @Query("SELECT new com.ssafy.eureka.domain.statistics.dto.CardOwnershipDto(co.cardId, c.cardName, cb.info, c.imagePath, c.imgAttr, co.ownershipCount) " +
+            "FROM CardOwnershipOverviewEntity co " +
+            "JOIN CardEntity c ON co.cardId = c.cardId " +
+            "JOIN CardBenefitEntity cb ON c.cardId = cb.cardId " +
             "WHERE co.createdDate = :date " +
             "ORDER BY co.ownershipCount DESC LIMIT 10")
     List<CardOwnershipDto> findCardOwnershipOverviews(@Param("date") LocalDate date);
