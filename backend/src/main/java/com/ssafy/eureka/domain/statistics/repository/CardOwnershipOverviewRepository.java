57a59ae599f96794502fb7f0125f17152d408eba
kdw <hide4321@naver.com>
2024-04-04 09:11:45 +0900

    Fix: CardOwnershipOverviewRepository
    Change-Id: Ie755116a1b9ae944e12116be9c3c429aeeec615c

--- backend/src/main/java/com/ssafy/eureka/domain/statistics/repository/CardOwnershipOverviewRepository.java
+++ backend/src/main/java/com/ssafy/eureka/domain/statistics/repository/CardOwnershipOverviewRepository.java
@@ -13,19 +13,19 @@ import java.util.List;
 @Repository
 public interface CardOwnershipOverviewRepository extends JpaRepository<CardOwnershipOverviewEntity, Integer> {
 
-//    @Query("SELECT new com.ssafy.eureka.domain.statistics.dto.CardOwnershipDto(co.cardId, c.cardName, c.imagePath, c.imgAttr, co.ownershipCount) " +
-//            "FROM CardOwnershipOverviewEntity co JOIN CardEntity c ON co.cardId = c.cardId " +
-//            "WHERE co.createdDate = :date " +
-//            "ORDER BY co.ownershipCount DESC LIMIT 10")
-//    List<CardOwnershipDto> findCardOwnershipOverviews(@Param("date") LocalDate date);
-
-    @Query("SELECT new com.ssafy.eureka.domain.statistics.dto.CardOwnershipDto(co.cardId, c.cardName, cb.info, c.imagePath, c.imgAttr, co.ownershipCount) " +
-            "FROM CardOwnershipOverviewEntity co " +
-            "JOIN CardEntity c ON co.cardId = c.cardId " +
-            "JOIN CardBenefitEntity cb ON c.cardId = cb.cardId " +
+    @Query("SELECT new com.ssafy.eureka.domain.statistics.dto.CardOwnershipDto(co.cardId, c.cardName, c.imagePath, c.imgAttr, co.ownershipCount) " +
+            "FROM CardOwnershipOverviewEntity co JOIN CardEntity c ON co.cardId = c.cardId " +
             "WHERE co.createdDate = :date " +
             "ORDER BY co.ownershipCount DESC LIMIT 10")
     List<CardOwnershipDto> findCardOwnershipOverviews(@Param("date") LocalDate date);
+
+//    @Query("SELECT new com.ssafy.eureka.domain.statistics.dto.CardOwnershipDto(co.cardId, c.cardName, cb.info, c.imagePath, c.imgAttr, co.ownershipCount) " +
+//            "FROM CardOwnershipOverviewEntity co " +
+//            "JOIN CardEntity c ON co.cardId = c.cardId " +
+//            "JOIN CardBenefitEntity cb ON c.cardId = cb.cardId " +
+//            "WHERE co.createdDate = :date " +
+//            "ORDER BY co.ownershipCount DESC LIMIT 10")
+//    List<CardOwnershipDto> findCardOwnershipOverviews(@Param("date") LocalDate date);
 
     @Query("SELECT MAX(co.createdDate) FROM CardOwnershipOverviewEntity co")
     LocalDate findLatestCreatedDate();
